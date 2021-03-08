9d8aeaff4d66a46eb1855ad56c96298e5db6e131
christian <christian.koboldt@dotecon.com>
2021-03-08 11:20:02 +0100

    Adjusting tests to use instant cloud integration and renamin tests so that the surefire plugin will pick them up

---
+++ src/test/java/org/ojalgo/optimisation/solver/gurobi/InstantCloudAPIConfig.java
@@ -0,0 +1,8 @@
+package org.ojalgo.optimisation.solver.gurobi;
+
+public class InstantCloudAPIConfig {
+
+        public static String ACCESSKEY = "Your Access Key Here";
+        public static String SECRET = "Your Secret Here";
+
+}
